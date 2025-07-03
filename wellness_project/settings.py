4a5bef61aeb29522243e46f2e5dcd70739166ff4
Kristjan Kelk <60584291+KristjanKelk@users.noreply.github.com>
2025-07-04 00:47:59 +0300

    Add pagination controls and remove recipe search limit

--- wellness_project/settings.py
+++ wellness_project/settings.py
@@ -203,8 +203,7 @@ REST_FRAMEWORK = {
         'user': '100/minute',
         'anon': '20/minute',
     },
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'DEFAULT_PAGINATION_CLASS': 'utils.pagination.StandardResultsSetPagination',
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
